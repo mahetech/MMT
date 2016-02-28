7202c2448d11a932cc518ea4a76e7a54b18c7cde
Mahe Tech <mahetech0@gmail.com>
2016-02-29 01:56:40 +0800

    28-Feb-2016

--- MemberManagement/src/main/java/com/sbfc/member/management/configuration/AppInitializer.java
+++ MemberManagement/src/main/java/com/sbfc/member/management/configuration/AppInitializer.java
@@ -1,26 +1,15 @@
 package com.sbfc.member.management.configuration;
 
-import java.util.List;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import com.sbfc.member.management.common.Common;
-import com.sbfc.member.management.dao.IMemberDao;
-import com.sbfc.member.management.model.GlobalConstant;
-
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-	// TODO: to be replaced by appropriate service class
-	@Autowired
-	private IMemberDao memberDao;
 
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
@@ -36,14 +25,6 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 				new DispatcherServlet(rootContext));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/");
-
-		if (memberDao != null) {
-
-			List<GlobalConstant> constList = memberDao.retrieveAllConstants();
-			Common.loadConstants(constList);
-		} else {
-			System.err.println("Could not load the global constants");
-		}
 	}
 
 	@Override
